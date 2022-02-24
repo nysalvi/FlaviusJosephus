7ce758e30dbef66c67fd74ad4437e23c46eb78f7
Ântony Salvi <nysalvi20@gmail.com>
2022-02-24 15:01:38 -0300

    .

--- FlaviusJosephus.ConsoleApp/Program.cs
+++ FlaviusJosephus.ConsoleApp/Program.cs
@@ -4,48 +4,69 @@ namespace FlaviusJosephus.ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             int casosDeTestes;
-            while (true) {
+            while (true)
+            {
                 Console.WriteLine("Digite a quantidade de casos de testes : ");
-                if (int.TryParse(Console.ReadLine(), out casosDeTestes) && casosDeTestes > 0 && casosDeTestes < 31);
-                    break;
+                if (int.TryParse(Console.ReadLine(), out casosDeTestes) && casosDeTestes > 0 && casosDeTestes < 31) ;
+                break;
             }
-            int pessoas, tamanhoSalto;
+            int pessoas = 0, tamanhoSalto;
             bool[] pessoasMortas;
 
             char[] input;
+            string temp = "";
+            int casoAtual = 1;
 
-            while (casosDeTestes > 0) {
+            while (casoAtual <= casosDeTestes)
+            {
                 Console.WriteLine("Digite a quantidade de pessoas no círculo e o tamanho do salto: ");
                 input = Console.ReadLine().ToCharArray();
-
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] != ' ')
+                        temp += input[i];
+                    else
+                    {
+                        pessoas = int.Parse(temp);
+                        temp = "";
+                    }
+                }
+                tamanhoSalto = int.Parse(temp);
                 pessoasMortas = new bool[pessoas];
-
+                temp = "";
                 int contador = tamanhoSalto - 1;
-                int saltoAtual = 0;
                 pessoasMortas[contador] = true;
-
-                while (true) {
-                    if (pessoasMortas[contador] == false) {
+                int pessoaRestante = pessoas - 1;
+                int saltoAtual = 0;
+                while (true)
+                {
+                    if (pessoasMortas[contador] == false)
+                    {
                         saltoAtual++;
+                        if (saltoAtual == tamanhoSalto)
+                        {
+                            pessoasMortas[contador] = true;
+                            pessoaRestante--;
+                            if (pessoaRestante == 1)
+                            {
+                                Console.WriteLine("Caso " + casoAtual + ": " +
+                                (Array.IndexOf(pessoasMortas, false) + 1));
+                                break;
+                            }
+                            saltoAtual = 0;
+                        }
                     }
-                    if (saltoAtual == tamanhoSalto) {
-                        pessoasMortas[contador] = true;
-                        saltoAtual = 0;
-                    }
-                    if (contador + tamanhoSalto >= pessoasMortas.Length)
-                        contador = tamanhoSalto - (pessoasMortas.Length - contador) - 1;
-                    else 
-                        contador += tamanhoSalto;
-
                     contador++;
                     if (contador == pessoasMortas.Length)
                         contador = 0;
                 }
+                casoAtual++;
 
-                casosDeTestes--;
             }
+            Console.ReadLine();
         }
     }
 }
